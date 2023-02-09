3a1c7842b7873045d38e0dcba3f70a317fbb8bdb
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-02-09 21:24:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your code here
+ function majorityElementChecker(arr)
+    {
+        let map = new Map();
+ 
+        for(let i = 0; i < arr.length; i++) {
+            if (map.has(arr[i])) {
+                    let count = map.get(arr[i]) +1;
+                    if (count > arr.length /2) {
+                      //  document.write("Majority found :- " + arr[i]);
+                        return arr[i];
+                    } else
+                        map.set(arr[i], count);
+ 
+            }
+            else
+                map.set(arr[i],1);
+            }
+            return 1;
+            //document.write(" No Majority element");
+    }
+
+module.exports = majorityElementChecker;
